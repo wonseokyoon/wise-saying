08411be03bb052f6656bba5b4236b84cfe52283e
wonseokyoon <01088115030a@gmail.com>
2024-12-23 15:21:54 +0900

    ffeature register

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App{
+    public void run(){
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("==명언 앱==");
+        System.out.println("명언: ");
+        scanner.nextLine();
+
+        System.out.println("작가: ");
+        System.out.println("1번 명언이 등록되었습니다.");
+        System.out.println("명령) 종료");
+
+        //if(scanner=="")
+
+
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,11 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("test1");
-
+        App app=new App();
+        app.run();
     }
+
 }
