7e04f895ca23dcbebea6253b8d71b75ee7edb564
wonseokyoon <01088115030a@gmail.com>
2024-12-23 16:12:35 +0900

    feature numbering #3,4

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,22 +5,25 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App{
+    private static int id=1;
     public void run(){
         Scanner scanner=new Scanner(System.in);
         List<Quote> quotes=new ArrayList<>();
 
         while(true){
+            System.out.println("==명언 앱==");
             System.out.print("명령) ");
-            String command=scanner.nextLine();
+            String command=scanner.nextLine().trim();
 
             if(command.equals("등록")){
                 System.out.print("명언: ");
-                String text=scanner.nextLine();
-                System.out.print("작가: ");
-                String author=scanner.nextLine();
+                String text=scanner.nextLine().trim();
 
-                quotes.add(new Quote(text,author));
-                System.out.println("1번 명언이 등록되었습니다");
+                System.out.print("작가: ");
+                String author=scanner.nextLine().trim();
+
+                quotes.add(new Quote(id++,text,author));
+                System.out.println(id-1+"번 명언이 등록되었습니다");
             } else if (command.equals("종료")) {
                 break;
             }
--- src/main/java/org/example/Quote.java
+++ src/main/java/org/example/Quote.java
@@ -3,15 +3,19 @@ package org.example;
 public class Quote {
     private String text;
     private String author;
+    private int id;
 
-    public Quote(String text,String author){
+    public Quote(int id,String text,String author){
+        this.id=id;
         this.text=text;
         this.author=author;
     }
 
     @Override
     public String toString(){
-        return "명언 " + text + "\n작가: "+ author;
+        return id+ "번 "
+                +"명언 " + text
+                + "\n작가: "+ author;
     }
 
 }
