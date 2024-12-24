ddf144edef09f2c040acb3267b200cc55f835172
wonseokyoon <01088115030a@gmail.com>
2024-12-24 15:19:38 +0900

    feature saveFile/saveLastId

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,16 +1,20 @@
 package org.example;
 
-import java.sql.SQLOutput;
+import org.json.JSONObject;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class App{
     private static int id=1;
+    List<Quote> quotes=new ArrayList<>();
+    // 명언 저장 할 위치
+    private static final String path="db/wiseSaying";
+
     public void run(){
         Scanner scanner=new Scanner(System.in);
-        List<Quote> quotes=new ArrayList<>();
-
         while(true) {
             System.out.println("==명언 앱==");
             System.out.print("명령) ");
@@ -19,6 +23,9 @@ public class App{
             if (command.equals("등록")) {
                 Register register=new Register(quotes);
                 register.start(id);
+                Quote newQuote=quotes.get(quotes.size()-1); //등록한 quote를 새로운 객체에 저장
+                saveFile(newQuote);
+                saveLastId(id);
                 id++;
             } else if (command.equals("종료")) {
                 break;
@@ -36,5 +43,32 @@ public class App{
             }
         }
         scanner.close();
+    }
+
+    private void saveLastId(int id) {   //마지막 id 저장
+        try{
+            FileWriter writer=new FileWriter(path+"/lastId.txt");
+            writer.write(String.valueOf(id));
+            writer.close();
+        }catch (IOException e){
+            System.out.println("오류"+e.getMessage());
+        }
+    }
+
+    public void saveFile(Quote quote){  // 파일 저장
+        JSONObject json=new JSONObject();   //Json 객체 생성
+        json.put("id",quote.getId());
+        json.put("text",quote.getText());
+        json.put("author",quote.getAuthor());
+
+        String jsonPath=path+"/"+quote.getId()+".json";
+        try{
+            FileWriter writer=new FileWriter(jsonPath);
+            writer.write(json.toString(4)); //들여쓰기(4번) 포함해서 json문자열 작성
+            writer.close();  //리소스 해제
+        }catch (IOException e){
+            System.out.println("오류"+e.getMessage());
+        }
+
     }
 }
