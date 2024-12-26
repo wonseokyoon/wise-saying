f9d8b8cd521854f3dc7b18e9796c9940c99c8872
wonseokyoon <01088115030a@gmail.com>
2024-12-26 16:17:27 +0900

    create Service

--- src/main/java/org/example/service/WiseSayingService.java
+++ src/main/java/org/example/service/WiseSayingService.java
@@ -1,9 +1,132 @@
 package org.example.service;
 
-
+import org.example.entity.WiseSaying;
 import org.example.repository.WiseSayingRepository;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WiseSayingService implements WiseSayingRepository{
+    private List<WiseSaying> sayings=new ArrayList<>();
+    private static final String path="db/wiseSaying";
+    private static int lastId=1;
+    public WiseSayingService(){
+        loadWiseSayings();
+    }
+
+    private void loadWiseSayings(){
+        File folder=new File(path);
+        File[] files=folder.listFiles();
+
+        if(files!=null){
+            for(File file:files){
+                if(file.isFile() && file.getName().endsWith(".json")
+                        && !file.equals("data.json")){
+                    try {
+                        String contents=new String(Files.readAllBytes(file.toPath()));
+                        JSONObject json=new JSONObject(contents);
+
+                        int id= json.getInt("id");
+                        String text= json.getString("text");
+                        String author= json.getString("author");
+
+                        sayings.add(new WiseSaying(id,text,author));
+                        lastId=Math.max(lastId,id+1);
+                    }catch (IOException e){
+                        System.out.println("파일 로드 실패"+e.getMessage());
+                    }catch (JSONException e){
+                        System.out.println("Json 오류"+e.getMessage());
+                    }
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public List<WiseSaying> findAll() {
+        return sayings;
+    }
+
+    @Override
+    public void register(String text,String author) {
+        // 리스트에 저장
+        WiseSaying wiseSaying=new WiseSaying(lastId++,text,author);
+        sayings.add(wiseSaying);
+        saveLastId(lastId);
+        // 파일 저장
+        saveToFile(wiseSaying);
+
+    }
+
+    private void saveToFile(WiseSaying wiseSaying) {
+        JSONObject json=new JSONObject();
+        json.put("id",wiseSaying.getId());
+        json.put("text",wiseSaying.getText());
+        json.put("author",wiseSaying.getAuthor());
+        String jsonPath=path+"/"+wiseSaying.getId()+".json";
+        try (FileWriter writer=new FileWriter(jsonPath)){
+            writer.write(json.toString(4));
+        }catch (IOException e){
+            System.out.println("저장 오류: "+e.getMessage());
+        }
+    }
+
+    @Override
+    public void modify(WiseSaying wiseSaying) {
+        for(WiseSaying saying:sayings){
+            if(saying.getId()==wiseSaying.getId()){
+                saying.setText(wiseSaying.getText());
+                saying.setAuthor(wiseSaying.getAuthor());
+                saveToFile(saying);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        //리스트에서 제거
+        sayings.removeIf(saying -> saying.getId()==id);
+        //파일 삭제
+        File file=new File(path+"/"+id+".json");
+        if(file.exists()){
+            file.delete();
+        }
+    }
+
+    @Override
+    public void build() {
+        JSONArray jsonArray=new JSONArray();
+        for(WiseSaying saying:sayings){
+            JSONObject json=new JSONObject();
+            json.put("id",saying.getId());
+            json.put("text",saying.getText());
+            json.put("author",saying.getAuthor());
+            jsonArray.put(json);
+        }
+        try(FileWriter writer=new FileWriter(path+"/data.json")){
+            writer.write(jsonArray.toString(4));
+        }catch (IOException e){
+            System.out.println("Json 빌드 오류: "+e.getMessage());
+        }
+    }
+    @Override
+    public void saveLastId(int id){
+        try{
+            FileWriter writer=new FileWriter(path+"/lastId.txt");
+            writer.write(String.valueOf(id));
+            writer.close();
+        }catch (IOException e){
+            System.out.println("ID 저장 오류: "+e.getMessage());
+        }
+    }
 
 }
