d911025ba40186b1ab9a0ad4e01ca130b59570b4
wonseokyoon <01088115030a@gmail.com>
2024-12-26 14:27:57 +0900

    create WiseSaying Entity

--- src/main/java/org/example/entity/WiseSaying.java
+++ src/main/java/org/example/entity/WiseSaying.java
@@ -1,4 +1,33 @@
 package org.example.entity;
 
 public class WiseSaying {
+    private int id;
+    private String text;
+    private String author;
+
+    public WiseSaying(int id,String text,String author){
+        this.id=id;
+        this.text=text;
+        this.author=author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
