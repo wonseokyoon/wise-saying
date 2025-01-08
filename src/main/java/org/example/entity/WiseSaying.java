aad141a711b9a11954e43ff10487619c1a6c3f80
wonseokyoon <01088115030a@gmail.com>
2025-01-08 17:55:58 +0900

    refactor wiseSaying

--- src/main/java/org/example/entity/WiseSaying.java
+++ src/main/java/org/example/entity/WiseSaying.java
@@ -1,5 +1,10 @@
 package org.example.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class WiseSaying {
     private int id;
     private String text;
@@ -9,26 +14,6 @@ public class WiseSaying {
         this.id=id;
         this.text=text;
         this.author=author;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setText(String text) {
-        this.text = text;
     }
 
     @Override
